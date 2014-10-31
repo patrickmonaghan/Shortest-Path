0d8edbfe1acb2675ceb10476ce8469235c04779a
unknown <jwb09164@CIS-KELVIN-31.ds.strath.ac.uk>
2014-10-31 13:35:29 +0000

    Basic file reading and graph building added in

--- classes.py
+++ classes.py
@@ -10,6 +10,9 @@ class Graph():
 		# Create a private dictionary for mapping IDs to names
 		self.__id_mapping__ = {}
 		
+	def print_(self):
+		print(self.__graph__)
+		
 		
 	def add_node(self, name, id):
 		""" This method adds a node to the graph and notes the ID of the node
@@ -30,6 +33,10 @@ class Graph():
 	def add_edge(self, name, manager):
 		""" This method adds an edge between nodes, representing an employee
 		    and his/her manager """
+		# If this employee does not have a manager, don't add an edge
+		if not manager:
+			return None
+			
 		# We will receive the manager as an ID, so find the name
 		if manager in self.__id_mapping__:
 			manager_name = self.__id_mapping__[manager]
@@ -39,6 +46,31 @@ class Graph():
 			# Make sure we don't add duplicate edges
 			if manager_name not in self.__graph__[name]:
 				self.__graph__[name].append(manager_name)
-			If name not in self.__graph__[manager_name]:
+			if name not in self.__graph__[manager_name]:
 				self.__graph__[manager_name].append(name)
 		
+	def find_path(self, start, end, path):
+		""" This method finds the path between two nodes in the graph """
+		path = path + [start]
+		if start == end:
+			# If the start and end nodes specified are the same, no need to
+			# find a path
+			return path
+		
+		if start not in self.__graph__:
+			# The specified start node doesn't exist
+			return None
+		
+		if end not in self.__graph__:
+			# The specified end node doesn't exist
+			return None
+			
+		for node in self.__graph__[start]:
+			if node not in path:
+				newpath = find_path(node, end, path)
+				if newpath:
+					#If we have found a path, return it
+					return newpath
+		
+		# And if no path is found, return nothing
+		return None
--- hierarchy.py
+++ hierarchy.py
@@ -2,11 +2,61 @@
 # Developed using Python 2.7
 
 import sys
+import os.path
 from classes import Graph
+
+USAGE = "Usage: python2.7 hierarchy.py [File] [Start] [End]"
 			 
 def main(args):
-	print(args)
+	""" This is the main method that is called when the app is started """
+	# Get the filenames from the arguments
+	try:
+		filename = args[1]
+		start = args[2]
+		end = args[3]
+	except Exception:
+		sys.exit(USAGE)
+	
+	graph = build_graph(filename)
+	
+def build_graph(filename):
+	""" This method takes the text file and builds the graph """
+	if not os.path.isfile(filename):
+		# Check the file exists
+		sys.exit("%s is not a valid file." % filename)
+	
+	# Create the graph file
+	graph = Graph()
+	# Open the file and loop through the data
+	f = open(filename, 'r')
+	# Read the column headers
+	header = f.readline()
+	
 
+	file_data = []
+	for line in f:
+		# Split the line into rows
+		line_data = line.split("|")
+		# Take in the appropriate data, and remove trailing and leading spaces
+		id = line_data[1].lstrip().rstrip()
+		name = line_data[2].lstrip().rstrip()
+		manager = line_data[3].lstrip().rstrip()
+		data = [id, name, manager]
+		file_data.append(data)
+	
+	# Now loop through all the data and create nodes. Edges are not added at 
+	# this point as we cannot guarantee the order in which we get data in
+	for data in file_data:
+		id = data[0]
+		name = data[1]
+		graph.add_node(name, id)
+	
+	# Now we have all nodes in, we can create the edges
+	for data in file_data:
+		name = data[1]
+		manager = data[2]
+		graph.add_edge(name, manager)
+		
 	
 if __name__ == "__main__":
 	main(sys.argv)
