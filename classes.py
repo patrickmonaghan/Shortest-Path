34a9bebc9d87b0f59f6d73402f11852f83fab965
unknown <jwb09164@CIS-KELVIN-31.ds.strath.ac.uk>
2014-10-31 14:50:09 +0000

    Program now outputs appropriately

--- classes.py
+++ classes.py
@@ -9,6 +9,9 @@ class Graph():
 		self.__name_mapping__ = {}
 		# Create a private dictionary for mapping IDs to names
 		self.__id_mapping__ = {}
+		# Create a private dictionary for mapping employees to managers
+		self.__manager_mapping__ = {}
+		
 		
 	def add_node(self, name, id):
 		""" This method adds a node to the graph and notes the ID of the node
@@ -17,7 +20,6 @@ class Graph():
 			# If the name doesn't already exist in the graph, add it as a 
 			# node
 			self.__graph__[name] = []
-		
 		if name not in self.__name_mapping__:
 			# If the name isn't already mapped to an ID, map it
 			self.__name_mapping__[name] = id
@@ -26,7 +28,7 @@ class Graph():
 			# If the ID isn't already mapped to an ID, map it
 			self.__id_mapping__[id] = name
 
-	def add_edge(self, name, manager):
+	def add_edge(self, id, name, manager):
 		""" This method adds an edge between nodes, representing an employee
 		    and his/her manager """
 		# If this employee does not have a manager, don't add an edge
@@ -36,6 +38,13 @@ class Graph():
 		# We will receive the manager as an ID, so find the name
 		if manager in self.__id_mapping__:
 			manager_name = self.__id_mapping__[manager]
+			
+		# Keep a track of the direction of the edges by keeping a dictionary
+		# of managers.
+		if manager not in self.__manager_mapping__:
+			self.__manager_mapping__[manager] = []
+		if id not in self.__manager_mapping__[manager]:
+			self.__manager_mapping__[manager].append(id)
 		
 		# If both the employee and manager exist in the graph, add the edge
 		if name in self.__graph__ and manager_name in self.__graph__:
@@ -45,12 +54,19 @@ class Graph():
 			if name not in self.__graph__[manager_name]:
 				self.__graph__[manager_name].append(name)
 				
+				
 	def node_id(self, name):
 		""" This method returns the ID of any given node """
 		if name in self.__name_mapping__:
 			return self.__name_mapping__[name]
 		else:
 			return None
+			
+	def get_manager(self, id):
+		for mgr, ids in self.__manager_mapping__.iteritems():
+			if id in ids:
+				return mgr
+		return None
 		
 	def find_path(self, start, end, path=[]):
 		""" This method finds the path between two nodes in the graph """
--- hierarchy.py
+++ hierarchy.py
@@ -33,7 +33,6 @@ def build_graph(filename):
 	f = open(filename, 'r')
 	# Read the column headers
 	header = f.readline()
-	
 
 	file_data = []
 	for line in f:
@@ -55,18 +54,32 @@ def build_graph(filename):
 	
 	# Now we have all nodes in, we can create the edges
 	for data in file_data:
+		id = data[0]
 		name = data[1]
 		manager = data[2]
-		graph.add_edge(name, manager)
+		graph.add_edge(id, name, manager)
 		
 	return graph
 	
 def path_to_string(graph, path):
 	""" This method takes the graph and a path between two nodes, and formats
 	    the path into the required output format """
-	for node in path:
-		id = graph.node_id(node)
-		print("%s (%s)" % (node, id))
+	direction = "-->"
+	last_manager = None
 	
+	# Print the first element to start the list
+	start = path[0]
+	id = graph.node_id(start)
+	last_manager = graph.get_manager(id)
+	path_string = "%s (%s)" % (start, id)
+	
+	for node in path[1:]:
+		id = graph.node_id(node)
+		if id != last_manager:
+			direction = "<--"
+		last_manager = graph.get_manager(id)
+		path_string = "%s %s %s (%s)" % (path_string, direction, node, id)
+		
+	return path_string
 if __name__ == "__main__":
 	main(sys.argv)
